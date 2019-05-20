250277a2040af4f3a34c88b2c98c30df3f4e62d4
Sebastián <hola@sebastian.digital>
2019-05-20 19:46:10 -0300

    Updating CV, adding https, etc.

--- Personal.Site/wwwroot/js/personal.js
+++ Personal.Site/wwwroot/js/personal.js
@@ -163,7 +163,7 @@ $(function(){
                     youtube: {
                         index: 'youtube.com/',
                         id: 'v=',
-                        src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+                        src: 'https://www.youtube.com/embed/%id%?autoplay=1'
                     }
                 }
             }
